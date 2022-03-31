5fbfa085b2a1c85100cb4df0e68f41fa99560e24
Диана Бережная <Lacshri@yandex.ru>
2022-04-01 00:04:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1200;
+        int oneMile = 20;
+        int total = priceTicket / oneMile;
+        System.out.println(total);
+    }
+}
